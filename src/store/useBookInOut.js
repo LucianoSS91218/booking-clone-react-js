4dd1ad6d11cacdb8d51d84e211196b933685c36d
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2025-10-20 20:05:06 -0300

    Create useBookInOut.js

---
+++ src/store/useBookInOut.js
@@ -0,0 +1,119 @@
+import { create } from "zustand";
+import { devtools } from "zustand/middleware";
+
+export const useBookInOut = create()(
+  devtools(
+    (set, get) => ({
+      mode: null,
+      flexibleDate: [],
+      displayMonths: [],
+      checkInDate: { day: null, month: null, year: null },
+      checkOutDate: { day: null, month: null, year: null },
+      timedays: {
+        dateOption: "Fechas Exactas",
+        flexibleOption: null,
+        mode: null,
+      },
+      howmanytravelers: {
+        group_adults: 1,
+        group_children: 0,
+        rooms: 1,
+      },
+      isActivePets: false,
+      handleCheckIn: (day, month, year) => {
+        const { checkInDate } = get();
+        let newState = {};
+
+        if (checkInDate.day === null) {
+          newState = {
+            mode: "calendar",
+            checkInDate: { day: day, month: month, year: year },
+            flexibleDate: [],
+            displayMonths: [],
+          };
+        } else {
+          newState = {
+            mode: null,
+            checkInDate: { day: null, month: null, year: null },
+            checkOutDate: { day: null, month: null, year: null },
+          };
+        }
+
+        set(() => newState);
+      },
+
+      handleCheckOut: (day, month, year) => {
+        const { checkInDate, checkOutDate } = get();
+        let newState = {};
+
+        if (checkInDate.day && checkOutDate.day === null) {
+          newState = {
+            mode: "calendar",
+            checkOutDate: { day: day, month: month, year: year },
+            flexibleDate: [],
+            displayMonths: [],
+          };
+        }
+
+        set(() => newState);
+      },
+
+      handleFlexibleMonths: (value, month, year) => {
+        const { flexibleDate, displayMonths } = get();
+        let newState = [];
+        let otherState = [];
+        if (flexibleDate.includes(value)) {
+          newState = flexibleDate.filter((v) => v !== value);
+          otherState = displayMonths.filter(
+            (f) => f.month !== month && f.year !== year
+          );
+        } else if (flexibleDate.length < 3) {
+          newState = [...flexibleDate, value];
+          otherState = [...displayMonths, { month: month, year: year }];
+        }
+
+        set({
+          mode: "flexible",
+          flexibleDate: newState,
+          displayMonths: otherState,
+          checkInDate: { day: null, month: null, year: null },
+          checkOutDate: { day: null, month: null, year: null },
+        });
+      },
+
+      handleCalendarTime: (option) => {
+        set({
+          timedays: {
+            dateOption: option,
+            flexibleOption: null,
+            mode: "calendar",
+          },
+        });
+      },
+
+      handleFlexibleTime: (option) => {
+        set({
+          timedays: {
+            dateOption: null,
+            flexibleOption: option,
+            mode: "flexible",
+          },
+        });
+      },
+
+      handleIncreaseTravelersRooms: (fieldId, newValue) =>
+        set((state) => ({
+          howmanytravelers: {
+            ...state.howmanytravelers,
+            [fieldId]: newValue,
+          },
+        })),
+
+      togglePets: () => set((state) => ({ isActivePets: !state.isActivePets })),
+    }),
+
+    {
+      name: "bookingdate",
+    }
+  )
+);
