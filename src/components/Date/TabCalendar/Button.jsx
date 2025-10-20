5a0a145738c868434003beecc8df1b67fa093080
LucianoSS91218 <108553612+LucianoSS91218@users.noreply.github.com>
2025-10-20 20:20:05 -0300

    Create Button.jsx

---
+++ src/components/Date/TabCalendar/Button.jsx
@@ -0,0 +1,48 @@
+.searchbox-datepicker-calendar .prev-next-btn {
+  background: none;
+  border: none;
+
+  font-weight: 500;
+  color: #1a1a1a;
+  box-sizing: border-box;
+  
+}
+
+.prev-next-btn.arrowleft {
+  position: absolute;
+  top: 15px;
+  left: 30px;
+  z-index: 50;
+}
+
+.prev-next-btn.arrowright {
+  position: absolute;
+  top: 15px;
+  right: 30px;
+  z-index: 50;
+}
+
+.searchbox-datepicker-calendar .previous-month-btn .span-container-btnprevnext {
+  position: relative;
+  overflow: hidden;
+}
+
+.searchbox-datepicker-calendar
+  .previous-month-btn
+  .span-container-btnprevnext
+  .span-minicontainer-icon {
+
+  height: 20px;
+  width: 20px;
+}
+
+.searchbox-datepicker-calendar
+  .previous-month-btn
+  .span-minicontainer-icon
+  svg {
+  display: inline-block;
+  vertical-align: top;
+  height: 100%;
+  width: 100%;
+  cursor: pointer;
+}
